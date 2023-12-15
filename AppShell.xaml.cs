599080665431c11721b99227f9557b216133e1e5
Alex Caicedo Ramos <alex.caicedo@udla.edu.ec>
2023-12-15 00:05:03 -0500

    1er Commit: Implementada la funcionalidad básica de la página ProductosPage, que muestra una colección de productos con un botón para ver detalles. También se agregó la página DetallesProductoPage para visualizar información detallada de un producto específico al hacer clic en el botón correspondiente.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,11 @@
-﻿namespace AirsoftCore_App;
-
-public partial class AppShell : Shell
+﻿namespace AirsoftCore_App
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(Views.DetallesProductoPage), typeof(Views.DetallesProductoPage));
+        }
+    }
 }
